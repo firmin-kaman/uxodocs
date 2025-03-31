96302123b4c806ed398ad2d15a369532803b34ac
firmin-kaman <firmin-kaman@users.noreply.github.com>
2025-03-31 16:05:56 +0000

    deploy: b87fbb46ed1a14071277990100b0ade6c565bac4

---
+++ assets/js/5e95c892.5455cf71.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuxo_docs=self.webpackChunkuxo_docs||[]).push([[9647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var u=r(4164),c=r(1082),a=r(204),o=r(2831),d=r(1410),n=r(4848);function t(e){return(0,n.jsx)(c.e3,{className:(0,u.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,o.v)(e.route.routes)})})}}}]);
---
+++ assets/js/acecf23e.5c51d370.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuxo_docs=self.webpackChunkuxo_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
